afdfed11a940c3d00c94ee7b097f763f36216e2a
antoniocherneca <antumaster09@hotmail.com>
2023-10-01 01:52:16 -0300

    Primer commit

---
+++ AppFrontSofttek/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Data.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
+
+namespace AppFrontSofttek.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Ingresar(LoginDto loginDto)
+        {
+            return View("~/Views/Home/Index.cshtml");
+        }
+    }
+}
