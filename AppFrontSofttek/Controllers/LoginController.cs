daa9d579392e646cb85651abaf847993f3fbc1c2
antoniocherneca <antumaster09@hotmail.com>
2023-10-01 18:41:07 -0300

    login funcionando

--- AppFrontSofttek/Controllers/LoginController.cs
+++ AppFrontSofttek/Controllers/LoginController.cs
@@ -1,19 +1,36 @@
-﻿using Data.DTOs;
+﻿using Data.Base;
+using Data.DTOs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Razor.Compilation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Text.Json;
 
 namespace AppFrontSofttek.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly IHttpClientFactory _httpClient;
+
+        public LoginController(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Ingresar(LoginDto loginDto)
+        public async Task<IActionResult> Ingresar(LoginDto loginDto)
         {
-            return View("~/Views/Home/Index.cshtml");
+            var baseApi = new BaseApi(_httpClient);
+            var token = await baseApi.PostToApi("Login", loginDto);
+            var loginResult = token as OkObjectResult;
+            var objectResult = JsonConvert.DeserializeObject<Models.Login>(loginResult.Value.ToString());
+
+            // return RedirectToAction("Index", "Home");
+            return View("~/Views/Home/Index.cshtml", objectResult);
         }
     }
 }
---
+++ AppFrontSofttek/Models/Login.cs
@@ -0,0 +1,9 @@
+﻿namespace AppFrontSofttek.Models
+{
+    public class Login
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
